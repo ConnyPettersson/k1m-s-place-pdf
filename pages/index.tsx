125302c054adcdccf19c1b0bf3658d34f3ac71fa
Conny Pettersson <pedalconny@hotmail.com>
2024-12-28 13:57:49 +0100

    add startpage and styling in globals.css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
+'use client'
+import React, { useState } from 'react';
+import Image from 'next/image';
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className="flex flex-col items-center  justify-center min-h-screen bg-green-300 ">
+      <h1>K1M's place PDF - Start Page</h1>
+      <p>Welcome to TypeScript</p>
+    </div>
+  );
 }
